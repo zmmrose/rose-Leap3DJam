5a9b7c0f3f760a0080d9cb8d1cc7fa8091c8be73
Mengmei Zhou <rosemengmei@outlook.com>
2015-10-16 15:10:19 -0400

    First commit

---
+++ 3D Jam Project/Assets/ColliderDetector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderDetector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onCollisionEnter(Collision cl){
+		Debug.Log (cl.collider.gameObject.name);
+
+	}
+}
